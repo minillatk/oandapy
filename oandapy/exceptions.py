6511090da593a0c0497f567a793f87063734d352
Rafael Henter <rafael@henter.com.br>
2018-12-09 14:18:50 -0200

    Add tests to Oanda Core class

--- oandapy/exceptions.py
+++ oandapy/exceptions.py
@@ -1,40 +1,46 @@
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
-
-"""Exceptions module definition"""
+"""
+    Exceptions module definition
+"""
 
 
 class OandaError(Exception):
     """Oanda Error Exception Class"""
 
-    def __init__(self, status_code, resp_content):
+    def __init__(self, status_code='', resp_content=''):
         """OandaError Exception raised when server returns error.
 
         Args:
             status_code (str): Status code retrieved from the server.
             resp_content (dict): Response's body with more detailed info about
-                the problem that has occured.
+                                 the problem that has occurred.
 
         """
-
-        # TODO: Contact Oanda to inform about this bug
-        # These checks are necessary because right now, Oanda API is buggy
         if resp_content:
+            message = "OANDA server returned [{}] status code, ".format(status_code)
+
             if "errorCode" in resp_content:
-                code = resp_content["errorCode"]
-            else:
-                code = ""
+                message += "and API returned [{}] error code,".format(resp_content["errorCode"])
 
             if "errorMessage" in resp_content:
-                message = resp_content["errorMessage"]
-            else:
-                message = ""
+                message += "with message: ({})".format(resp_content["errorMessage"])
 
-            msg = ("OANDA server returned [{0}] error code, and API returned "
-                   "[{1}] error code, with message: ({2})"
-                   .format(status_code, code, message))
         else:
-            msg = ("OANDA server returned [{0}] error code, and no API "
-                   "response".format(status_code))
+            message = "OANDA server returned a internal server error"
 
-        super(OandaError, self).__init__(msg)
+        super(OandaError, self).__init__(message)
+
+
+class EnvironmentNotFound(Exception):
+    pass
+
+
+class AuthError(OandaError):
+    pass
+
+
+class NotFound(OandaError):
+    pass
+
+
+class ServerError(OandaError):
+    pass
